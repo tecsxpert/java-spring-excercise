b7f96d98d2991acfdde8c428421ecc15c15930c7
tecsxpert <dev@tecsxpert.xyz>
2026-04-21 10:34:45 +0530

    updated

---
+++ src/main/java/com/campuspe/javaspringexcercise/JavaSpringExcerciseApplication.java
@@ -0,0 +1,13 @@
+package com.campuspe.javaspringexcercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringExcerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringExcerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/campuspe/javaspringexcercise/JavaSpringExcerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campuspe.javaspringexcercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringExcerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
